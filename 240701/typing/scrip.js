f0630c3748fbe0e4c6328d42ae7448fd36fc20fe
KH-web38 <dhkim733@gmail.com>
2024-07-02 17:48:22 +0900

    240702

---
+++ 240701/typing/scrip.js
@@ -0,0 +1,18 @@
+const content =
+  "Hi!😄 I'm Dia, \n Front-End Developer. \n Welcome to My World :D";
+
+const text = document.querySelector(".text");
+
+let i = 0;
+
+const typing = () => {
+  let txt = content[i++];
+  text.innerHTML += txt === "\n" ? "<br/>" : txt;
+
+  if (i > content.length) {
+    text.textContent = "";
+    i = 0;
+  }
+};
+
+setInterval(typing, 250);
